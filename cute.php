9bb1e114984f0dad8dc2fd63c7c4bab0046b73ba
Ardin <ardin.ac@gmail.com>
2021-03-26 13:13:25 +0100

    Create cute.php

---
+++ cute.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Mother Cat Runs Into Burning Building 5 Times To Save Her Kittens</title>
+				<meta property="og:locale" content="en_US">
+				<meta property="og:type" content="article">
+				<meta property="og:title" content="Mother Cat Runs Into Burning Building 5 Times To Save Her Kittens">
+				<meta property="og:description" content="Back on March 30, 1996, Scarlett and her five kittens were in an abandoned garage which was being used illegally as a crack house in Brooklyn when a fire started. The New York City Fire Department extinguished it quickly. Once the fire was finally under control, David Giannelli, one of the...">
+				<meta property="og:url" content="https://welovecatsa.netlify.app/.netlify/functions/server/post/2908/?fbclid=IwAR3eVTHfkKGXBaM2L386Z4xCtpHaznWIM0splv_cmwBsZ05zxivcRnvjIjg&slug=2908%2F&utm=CFL2">
+				<meta property="og:site_name" content="We Love Animals">
+				<meta property="article:section" content="Animal">
+				<meta property="og:image" content="https://welovecatsa.netlify.app/uploads/2908.jpg">
+				<meta property="og:image:alt" content="Mother Cat Runs Into Burning Building 5 Times To Save Her Kittens">
+       
+       <style>
+          img { width: 100%; height: auto; }
+          ul { list-style-type: none; margin: 0; padding: 0; overflow: hidden; background-color: #333; }
+				  li {float: left; }
+				  li a { display: block; color: white; text-align: center; padding: 14px 16px; text-decoration: none; }
+				  li a:hover:not(.active) { background-color: #111; }
+				  .active { background-color: #4CAF50; }
+       </style>
+			</head>
+			<body style="background:#eee;"><div style="padding:20px;margin:30px auto;padding:20px;max-width:800px;background:white;box-shadow: 5px 5px 5px #888888;">
+      <div>
+				<ul>
+					<li><a href="#home">Home</a></li>
+					<li><a href="#news">News</a></li>
+					<li><a href="#contact">Contact</a></li>
+					<li style="float:right"><a class="active" href="#about">About</a></li>
+				</ul>
+			</div>
+        <h1>Mother Cat Runs Into Burning Building 5 Times To Save Her Kittens</h1>
+        <img style="width:100%;height:auto;" src="https://welovecatsa.netlify.app/uploads/2908.jpg">Back on March 30, 1996, Scarlett and her five kittens were in an abandoned garage which was being used illegally as a crack house in Brooklyn when a fire started. The New York City Fire Department extinguished it quickly. Once the fire was finally under control, David Giannelli, one of the firefighters on the scene, noticed Scarlett was carrying her kittens one by one away out of the garage.
+Scarlett was badly burned in the process of running into the fire so many times to save her babies. Her eyes were horribly blistered shut, her ears and paws burned, and her coat highly singed. Even worse, the majority of her facial hair had been burnt off.
+
+After finally saving all kittens, the mother touched each of her kittens with her nose to ensure they were all there and all alive as the blisters on her eyes made it impossible to see them at that time. Just when she made sure all of her babies were safe and sound, Scarlett allowed herself to collapse unconscious.
+
+Scarlett and her entire family were immediately taken to a veterinary clinic at the North Shore Animal League in Port Washington, New York where they were all treated. An unlucky kitten of the four died very shortly after from a virus a month after the fire. However, after three months of treatment and recovery, Scarlett and her surviving babies were all well healthy enough to be adopted.
+
+The heroine calico cat, who went through so much for her children, lost her battle with multiple illnesses after living with her adoptive family in Brooklyn, New York for over 12 years. On October 15th, 2008, Scarlett the cat, whose wonderful story of bravery, uncompromising love and triumph over all odds, passed away.
+<h3><strong>Back on March 30, 1996, Scarlett and her five kittens were in an abandoned garage which was being used illegally as a crack house in Brooklyn when a fire started.</strong></h3>
+<img class="size-full wp-image-2909 aligncenter" src="http://aubtu.pawsplanet.me/wp-content/uploads/2018/01/Mother-Cat-Runs-Into-Burning-Building-5-Times-To-Save-Her-Kittens-1.jpg" alt="" width="640" height="477" />
+<h3><strong>After finally saving all kittens, the mother touched each of her kittens with her nose to ensure they were all there and all alive as the blisters on her eyes made it impossible to see them at that time.</strong></h3>
+<img class="size-full wp-image-2910 aligncenter" src="http://aubtu.pawsplanet.me/wp-content/uploads/2018/01/Mother-Cat-Runs-Into-Burning-Building-5-Times-To-Save-Her-Kittens-2.jpg" alt="" width="476" height="345" />
+<h3><strong>The heroine calico cat, who went through so much for her children, lost her battle with multiple illnesses after living with her adoptive family in Brooklyn, New York for over 12 years.</strong></h3>
+<img class="size-full wp-image-2911 aligncenter" src="http://aubtu.pawsplanet.me/wp-content/uploads/2018/01/Mother-Cat-Runs-Into-Burning-Building-5-Times-To-Save-Her-Kittens-3.jpg" alt="" width="480" height="345" />
+<h3><strong>Watch this hero feline’s heartwarming story:</strong></h3>
+[embed]https://youtu.be/EXnngyov_qI[/embed]
+<h3><strong>We will never forget you, dear Scarlett.</strong></h3>
+<img class="size-full wp-image-2912 aligncenter" src="http://aubtu.pawsplanet.me/wp-content/uploads/2018/01/Mother-Cat-Runs-Into-Burning-Building-5-Times-To-Save-Her-Kittens-4.jpg" alt="" width="600" height="379" /></div>
+      </body>
+			</html>
